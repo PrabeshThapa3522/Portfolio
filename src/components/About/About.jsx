912d5b428212f50528627a5486ee330bac106f67
PrabeshThapa3522 <thapa.pranish7835@gmail.com>
2026-02-21 14:16:09 +0545

    Photo changed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {TypeAnimation} from 'react-type-animation';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/pt.jpg';
+import profileImage from '../../assets/photme.png';
 
 const About = () => {
   return (
